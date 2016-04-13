328d8f51956d4ef4d3f1cdb5d74af4f0c3639c59
Ben Lipton <ben.lipton@gmail.com>
2016-04-12 21:28:24 -0400

    Begin working on script to generate and update passwords

---
+++ rotatePasswords.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+"""Rotate the KittenCoin password on a machine and in the database.
+
+Rotate all passwords
+rotatePasswords.py
+
+Rotate just a few users
+rotatePasswords.py --users team1,team2
+
+Alternative config file (default is config.yml)
+rotatePasswords.py --config myconf.yml
+"""
+
+import argparse
+import random
+import string
+import sys
+import yaml
+import MySQLdb
+
+PASS_CHARS = string.ascii_letters + string.digits
+
+def RotateUser(username, hostname, db):
+  newpass = ''.join(random.choice(PASS_CHARS) for _ in xrange(20))
+  print newpass
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--users',
+      help='Comma-separated list of users whose passwords should be updated')
+  parser.add_argument('--config', default='config.yml',
+      help='Alternative config file to use (default is config.yml)')
+  args = parser.parse_args()
+
+  config_file = open(args.config)
+  config = yaml.safe_load(config_file)
+  config_file.close()
+
+  db = MySQLdb.connect(
+      host=config['hostname'],
+      user=config['user'],
+      port=config['port'],
+      passwd=config['password'])
+
+  if args.users:
+    users = args.users.split(',')
+  else:
+    users = config['users'].keys()
+
+  for user in users:
+    try:
+      hostname = config['users'][user]
+    except KeyError:
+      print >>sys.stderr, 'User %s not configured' % user
+      continue
+    RotateUser(user, hostname, db)
+
+if __name__ == '__main__':
+  main()
